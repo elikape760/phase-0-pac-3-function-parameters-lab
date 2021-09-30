bb66e38f91ef0dc11081fe906093602983fcd8bc
Elias <elikapeluschnik@gmail.com>
2021-09-29 23:28:45 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function introduction(name) {
+    return(`Hi, my name is ${name}.`);
+}
+
+  function introductionWithLanguage(name, language) {
+      return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+
+ function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+//   function introductionWithLanguageOptional(name = "Josh", language) {
+//       return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+//   }
+
+ 
+
+//   +Hi, my name is Gracie and I am learning to program in JavaScript.
+
+
+
+
+
+
